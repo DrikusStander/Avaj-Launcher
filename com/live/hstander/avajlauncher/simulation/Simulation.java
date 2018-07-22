c6e72a677dbe439305157bddab7c7afdebc12dd5
Drikus-PC\Drikus <standerhc@live.com>
2018-07-22 07:32:44 +0200

    something

--- com/live/hstander/avajlauncher/simulation/Simulation.java
+++ com/live/hstander/avajlauncher/simulation/Simulation.java
@@ -1,11 +1,41 @@
 
 package com.live.hstander.avajlauncher.simulation;
 
+import java.io.*;
+
 public class Simulation
 {
 	public static void main(String[] arg)
 	{
-		System.out.println("main");
-		System.exit(0);
+		try
+		{
+			System.out.println("main");
+			BufferedReader reader = null;
+			File file = new File(arg[0]);
+			reader = new BufferedReader(new FileReader(file));
+
+			String line;
+			String[] words;
+			int i = 0;
+			line = reader.readLine();
+			int count = Integer.parseInt(line.trim());
+			// int count = Integer.parseInt("123aa");
+			System.out.println(count);
+			while ((line = reader.readLine()) != null)
+			{
+				System.out.println(line);
+				words = line.split(" ");
+				for (String word : words)
+				{
+					System.out.println(word);
+				}
+			}
+			reader.close();
+			System.exit(0);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
